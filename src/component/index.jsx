9dc9e5539c38b4245695d715f1d5fc2777d1c393
sYashcoderz <syash4505@gmail.com>
2024-01-22 19:35:16 +0530

    updated code

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useEffect, useState } from "react"
-import { Col, Empty, Row, Spin } from "antd";
-import { Button, Form, Input } from 'antd';
+import { Col, Empty, Row, Button, Input, Select  } from "antd";
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 
 // Build a todo application
@@ -11,28 +11,55 @@ import { Button, Form, Input } from 'antd';
 // - List should update without refreshing the page
 
 const GitSearchDemo = () => {
-    const [form] = Form.useForm();
-    const [ isLoading, setIsLoading ] = useState();
+    const { Option } = Select;
     const [ todoList, setTodoList ] = useState([]);
+    const [isUpdate, setIsUpdate ] = useState(false);
+    const SelectOptions = ["pending", "completed", "inprogress"];
     const [ inputTodo, setInputTodo ] = useState(
-        { uid:0, status : 'pending', title:'' }
+        { title:'', description: '', status : 'pending' }
     );
 
-    // useEffect( () => {
-    //     localStorage.setItem('todoList', JSON.stringify(todoList));
-    //     const storedData = localStorage.getItem('todoList');
-    //     console.log("Stored Data ==>>", storedData);
-    //     setTodoList(storedData);
-    // }, [todoList]);
+    useEffect(() => {
+        const storedTodoList = JSON.parse(localStorage.getItem('todoList'));
+        console.log("Stored Todo List: ", storedTodoList);
+        setTodoList(storedTodoList);
+    }, []);
+    
+    useEffect(() => {
+        if (todoList.length > 0) {
+            localStorage.setItem('todoList', JSON.stringify(todoList));
+        }
+    }, [todoList]);
 
     const handleSubmit = () => {
-        setTodoList((prevList) => [ ...prevList, inputTodo]);
-        setInputTodo({});
-    }
+        if (inputTodo.title.trim() !== "") {
+        if (isUpdate) {
+            setTodoList((prevList) => {
+            const updatedList = prevList.map((item) =>
+                item.uid === inputTodo.uid
+                ? { ...item, title: inputTodo.title, description: inputTodo.description, status: inputTodo.status || 'pending' }
+                : item
+            );
+            return updatedList;
+            });
+            setInputTodo({});
+        } else {
+            setTodoList((prevList) => [
+            ...prevList,
+            { uid: new Date().getTime(), title: inputTodo.title, description: inputTodo.description, status: 'pending' },
+            ]);
+            setInputTodo({});
+        }
+        }
+    };
 
     const handleInputChange = (e) => {
         console.log("onChange ==", e.target.value);
-        setInputTodo({ ...inputTodo, title: e.target.value, uid: todoList.length });
+        setInputTodo({ ...inputTodo, title: e.target.value });
+    }
+
+    const handleDiscChange = (e) => {
+        setInputTodo({ ...inputTodo, description: e.target.value })
     }
 
     const handleDelete = (id) => {
@@ -41,53 +68,82 @@ const GitSearchDemo = () => {
     }
     
     const handleUpdate = (id) => {
-        const updatedTodo = todoList.filter((item) => { return item.uid === id});
-        // setInputTodo((prevTodo) => { ...prevTodo, {uid:0, status : 'pending', discription:'', title: e.target.value } );
+        setIsUpdate(true);
+        const updatedTodo = todoList.find((item) => item.uid === id);
+        setInputTodo(updatedTodo);
     }
 
-    console.log("INput to ", inputTodo);
+    const handleStatusChange = (value) => {
+        console.log("clk status ==",value);
+        setInputTodo({ ...inputTodo, status: value });
+    }
+
+    // console.log("INput to ", inputTodo);
     console.log("Submit to ", todoList);
 
 
     return (
         <Fragment>
-            <Row>
-                <Col span={24}>
-                    <Row style={{ height:'100%', border:'1px solid black', display:'flex', justifyContent:'center', alignItems:'center' }} >
-                        <Col span={24} style={{ border:'1px solid black', display:'flex', justifyContent:'center', alignItems:'center' }}>
-                            <Input placeholder="Enter to Insert" value={inputTodo.title} name="insert" onChange={handleInputChange} style={{ width: '200px' }} />
-                            
-                            <Button onClick={handleSubmit}>ADD</Button>
-                        </Col>
-                        <Col span={6} style={{ border:'1px solid black' }}>
-                            { todoList && todoList.map((item, index) => {
-                                return (
-                                    <>
-                                        { (item?.status === "inprogrss") ? <Row>
-                                            <h5>In Progress</h5>
-                                            <Col>{item?.title}</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleDelete(item?.uid) } } >delete</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleUpdate(item?.uid) } } >update</Col>
-                                        </Row> :
+            <Row style={{height:'100vh' , border:'1px solid green', display:'flex', justifyContent:'center', alignItems:'center' }} >
+                <h2>Welcom to Dice Todo</h2>
+                <Col xl={24} md={24} lg={24} sm={24} style={{ display:'flex', alignItems:'center', flexDirection:'column' }}>
+                    <Row>
+                        <Input placeholder="Enter to Insert Todo Item" value={inputTodo.title} name="insert" onChange={handleInputChange} style={{ width: '400px' }} required />
+                        <Input.TextArea type="textarea" value={inputTodo.description} onChange={handleDiscChange} placeholder="Enter Discription" style={{ width:'200px'}} required />
+                    </Row>
+                    <Row style={{ width:'40%', marginTop:'10px', display:'flex', justifyContent:'flex-end' }}>
+                        { isUpdate && 
 
-                                        (item?.status === "completed") ? <Row>
-                                            <h5>Completed</h5>
-                                            <Col>{item?.title}</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleDelete(item?.uid) } } >delete</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleUpdate(item?.uid) } } >update</Col>
-                                        </Row> : 
-
-                                        <Row>
-                                            <h5>Pending</h5>
-                                            <Col>{item?.title}</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleDelete(item?.uid) } } >delete</Col>
-                                            <Col style={{ padding: '5px', margin:'2px', cursor:'pointer' }} onClick={ () => { handleUpdate(item?.uid) } } >update</Col>
-                                        </Row>
-                                        }
-                                    </>
-                                )
-                            })}
+                        <Col>
+                            <Select onChange={handleStatusChange} defaultValue={'pending'} style={{ width: 200, marginRight:'500px' }}>
+                                {SelectOptions.map((stat) => (
+                                <Option key={stat} value={stat}>
+                                    {stat}
+                                </Option>
+                                ))}
+                            </Select>
                         </Col>
+                        }
+                        <Col>
+                            <Button type="primary" onClick={handleSubmit}>{ (isUpdate) ? "Update" : "ADD"}</Button>
+                        </Col>  
+                    </Row>
+                </Col>
+
+                <Col xl={24} md={24} lg={24} style={{ }}>
+                    <Row>
+                        { SelectOptions.map((status, index) => {
+                            return(
+                                <>
+                                    <Col xl={8} md={8} lg={8} key={index} style={{ border: "1px solid grey", minHeight: "500px" }}>
+                                        <h5>{status}</h5>
+                                        { todoList
+                                            .filter((item) => item?.status === status)
+                                            .map((item, index) => (
+                                            <Row key={index} style={{ gap: '5px' }} >
+                                                <Col style={{ fontSize:'18px', fontWeight: 700 }} >{item.title}</Col>
+                                                <Col style={{ fontSize:'14px', fontWeight: 400 }}>{item?.description}</Col>
+                                                <Button
+                                                    icon={<DeleteOutlined />}
+                                                    style={{ padding: "5px", margin: "2px", cursor: "pointer" }}
+                                                    onClick={() => {
+                                                        handleDelete(item.uid);
+                                                    }}
+                                                />
+                                                <Button
+                                                    icon={<EditOutlined />}
+                                                    style={{ padding: "5px", margin: "2px", cursor: "pointer" }}
+                                                    onClick={() => {
+                                                        handleUpdate(item.uid);
+                                                    }}
+                                                />
+                                            </Row>
+                                        ))}
+                                    </Col>
+                                </>
+                            )
+                        }) 
+                        }
                     </Row>
                 </Col>
             </Row>
